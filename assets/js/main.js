8d20d5170eb5af408bbef0b56131818b6e0ae83d
AndreaGazz <info.andreagazzaniga@gmail.com>
2023-02-07 17:13:36 +0100

    photo view noworking

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,14 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/*===== IMAGE VIEWER =====*/
+
+$(".images img").click(function(){
+    $("#full-image").attr("src", $(this).attr("src"));
+    $('#image-viewer').show();
+  });
+  
+  $("#image-viewer .close").click(function(){
+    $('#image-viewer').hide();
+  });
